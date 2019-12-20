55e7689851899a3022e3dddb623fcd6e28a4dfdf
julien <fettingerjulien@yahoo.fr>
2019-12-20 14:42:00 +0100

    save

--- src/store/terminatest/reducer/TestFunctional.ts
+++ src/store/terminatest/reducer/TestFunctional.ts
@@ -1,4 +1,4 @@
-import { ActionInterface, TestFunctionalInterface } from '../';
+import { ActionInterface, TestFunctionalInterface } from '..';
 
 export class TestFunctional implements TestFunctionalInterface {
   action:Array<ActionInterface>=[];
