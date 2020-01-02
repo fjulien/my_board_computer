fdbe6b6d145db4d5410ce27e00d13cf0dc99b209
fjulien <fettingerjulien@yahoo.fr>
2020-01-02 21:11:37 +0100

    clean termiantest

--- src/store/terminatest/index.ts
+++ src/store/terminatest/index.ts
@@ -2,3 +2,4 @@ export * from './reducer/Actions';
 export * from './action/testMaker';
 export * from './type/actionTest';
 export * from './type/testExecut';
+export * from './type/testFunctional';
